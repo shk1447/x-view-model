35624875071f5044929b2a4929b4903362bd38c0
석환 김 <sh2.kim@saigeresearch.ai>
2024-02-13 14:35:05 +0900

    visualize를 위한 json 준비

--- bin/xvm.ts
+++ bin/xvm.ts
@@ -8,12 +8,16 @@ program.version("0.0.1", "-v, --version").name("cli");
 const exts = ["js", "ts", "tsx", "jsx"];
 const excludes = ["node_modules"];
 const allowExtentions = [...exts, ...exts.map((d) => d.toUpperCase())];
-const findFiles = (dir: string, recursive: boolean) => {
+const findFiles = (
+  dir: string,
+  recursive: boolean,
+  callback: (file: any) => void
+) => {
   if (!path.basename(dir).startsWith(".")) {
     const list = fs.readdirSync(dir);
 
     list.forEach(function (file) {
-      file = dir + "\\" + file;
+      file = path.resolve(dir, "./" + file);
 
       const stat = fs.statSync(file);
 
@@ -23,7 +27,7 @@ const findFiles = (dir: string, recursive: boolean) => {
           !excludes.includes(path.basename(file))
         ) {
           /* Recurse into a subdirectory */
-          if (recursive) findFiles(file, recursive);
+          if (recursive) findFiles(file, recursive, callback);
         }
       } else {
         /* Is a file */
@@ -32,27 +36,113 @@ const findFiles = (dir: string, recursive: boolean) => {
 
         if (allowExtentions.includes(extension.replace(".", ""))) {
           const _file = {
-            fileName: fileName,
-            filePath: file,
-            fileExt: extension,
+            name: fileName,
+            path: file,
+            ext: extension,
           };
 
-          console.log(fileName);
+          callback(_file);
         }
       }
     });
   }
 };
 
+const registed_list = [];
+
+const used_list = [];
+
 program
-  .command("template <type>")
-  .usage("<type> --filename [filename] --path [path]")
+  .command("visualize")
   .description("템플릿을 생성합니다.")
-  .alias("tmpl")
+  .alias("vis")
   .option("-d, --directory [path]", "생성 경로를 입력하세요", ".")
-  .action((type, options) => {
-    findFiles(path.resolve(process.cwd(), options.directory), true);
-    // makeTemplate(type, options.filename, options.directory);
+  .action((options) => {
+    const basePath = path.resolve(process.cwd(), options.directory);
+    findFiles(basePath, true, (file) => {
+      const text = fs.readFileSync(file.path, "utf8");
+      const vf_result = text.match(/(?<=const\s).*(?=registViewFlow)/g);
+      const vm_result = text.match(/(?<=const\s).*(?=registViewModel)/g);
+
+      if (vf_result && vf_result.length > 0) {
+        registed_list.push(
+          ...vf_result.map((d) => {
+            return {
+              type: "vf",
+              name: d.split("=")[0].trim(),
+              file: file,
+            };
+          })
+        );
+      }
+
+      if (vm_result && vm_result.length > 0) {
+        registed_list.push(
+          ...vm_result.map((d) => {
+            return {
+              type: "vm",
+              name: d.split("=")[0].trim(),
+              file: file,
+            };
+          })
+        );
+      }
+
+      const use_vf_result = text.match(/(?<=useViewFlow\()[\s\S]*?(?=\))/g);
+      const use_vm_result = text.match(/(?<=useViewModel\()[\s\S]*?(?=\))/g);
+
+      if (use_vf_result && use_vf_result.length > 0) {
+        used_list.push(
+          ...use_vf_result.map((d) => {
+            const args = d
+              .replace(/\r?\n/g, "")
+              .replace(/\s/g, "")
+              .replace(/[\[\]]/g, "")
+              .split(",");
+            const vfName = args.shift();
+            const states = args;
+            return {
+              type: "vf",
+              name: vfName,
+              states: states,
+              file: file,
+            };
+          })
+        );
+      }
+
+      if (use_vm_result && use_vm_result.length > 0) {
+        used_list.push(
+          ...use_vm_result.map((d) => {
+            const args = d
+              .replace(/\r?\n/g, "")
+              .replace(/\s/g, "")
+              .replace(/[\[\]]/g, "")
+              .split(",");
+            const vfName = args.shift();
+            const states = args;
+            return {
+              type: "vm",
+              name: vfName,
+              states: states,
+              file: file,
+            };
+          })
+        );
+      }
+    });
+
+    fs.writeFileSync(
+      path.resolve(basePath, "./xvm_vis.json"),
+      JSON.stringify(
+        {
+          registed_list,
+          used_list,
+        },
+        null,
+        2
+      )
+    );
   });
 
 program.command("*", { noHelp: true }).action(() => {
